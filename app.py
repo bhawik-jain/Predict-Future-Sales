3b691b5766fb6357b66d6ce1258a62fa2f5fc2cf
Bhawik Jain <bhawikj2000@gmail.com>
2022-10-09 16:28:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import numpy as np
-from flask import Flask, request,render_template
 import pickle
+from flask import Flask, request,render_template
 import pandas as pd
-from xgboost import  XGBRegressor
+import xgboost
 
 app = Flask(__name__)
-model = pickle.load(open("XGBR_pkl", "rb"))
+
+with open('XGBR_pkl', 'rb') as files:
+    model = pickle.load(files)
+
 data = pickle.load(open('data_for_model.7z', 'rb'))
+
 #data = pd.read_pickle('data_for_model.7z')
 @app.route('/')
 def home():
@@ -21,11 +25,12 @@ def predict():
     feature2 = int(request.form.get('ITEMID'))
     query = data.loc[(data['shop_id']==feature1)&(data['item_id']==feature2)]
     if query.empty:
-        return render_template('index.html', prediction_text='Please enter valid SHOP ID and ITEM ID. Shop ID :{}/Item ID:{} not found.'.format(f1,f2))
+        return render_template('index.html', prediction_text='Please enter valid SHOP ID and ITEM ID. Shop ID :{}/Item ID:{} not found.'.format(feature1,feature2))
     else:
         #query=query.drop(['ID','shop_id','item_id'],axis=1)
         query = query[query["date_block_num"] > 2]
         query = query[query["date_block_num"] < 33]
+        
         prediction = model.predict(query)
 
         output = round(prediction[0].clip(0,20))
